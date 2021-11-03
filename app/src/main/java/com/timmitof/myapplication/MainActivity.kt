e5885a5e4af885cb832cef449be50fe758c1a8ec
timmitof <81027444+timmitof@users.noreply.github.com>
2021-11-03 15:50:02 +0600

    Добавил окно регистрации

--- app/src/main/java/com/timmitof/myapplication/MainActivity.kt
+++ app/src/main/java/com/timmitof/myapplication/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.timmitof.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonReg: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonReg = findViewById(R.id.regBtn)
+        buttonReg.setOnClickListener{
+            val intent = Intent(this, RegistrationActivity::class.java)
+            overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/timmitof/myapplication/RegistrationActivity.kt
@@ -0,0 +1,28 @@
+package com.timmitof.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegistrationActivity : AppCompatActivity() {
+
+    lateinit var buttonLog: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+        buttonLog = findViewById(R.id.logBtn)
+        buttonLog.setOnClickListener{
+            onBackPressed()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right)
+
+    }
+
+}

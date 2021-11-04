a400e3d43f24b9bb5fb0c4993676c348340e65b3
timmitof <81027444+timmitof@users.noreply.github.com>
2021-11-04 12:46:03 +0600

    Added home page

--- app/src/main/java/com/timmitof/myapplication/MainActivity.kt
+++ app/src/main/java/com/timmitof/myapplication/MainActivity.kt
@@ -8,16 +8,23 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
     lateinit var buttonReg: Button
+    lateinit var buttonLog: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         buttonReg = findViewById(R.id.regBtn)
+        buttonLog = findViewById(R.id.btnLogin)
         buttonReg.setOnClickListener{
-            val intent = Intent(this, RegistrationActivity::class.java)
+            val registration = Intent(this, RegistrationActivity::class.java)
             overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left)
-            startActivity(intent)
+            startActivity(registration)
+        }
+
+        buttonLog.setOnClickListener{
+            val login = Intent(this, HomeActivity::class.java)
+            startActivity(login)
         }
 
     }
--- app/src/main/java/com/timmitof/myapplication/RegistrationActivity.kt
+++ app/src/main/java/com/timmitof/myapplication/RegistrationActivity.kt
@@ -8,21 +8,25 @@ import android.widget.Button
 class RegistrationActivity : AppCompatActivity() {
 
     lateinit var buttonLog: Button
+    lateinit var btnLog:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        btnLog = findViewById(R.id.btnLogin2)
         buttonLog = findViewById(R.id.logBtn)
         buttonLog.setOnClickListener{
             onBackPressed()
         }
-    }
 
+        btnLog.setOnClickListener {
+            val login = Intent(this, HomeActivity::class.java)
+            startActivity(login)
+        }
+    }
     override fun onBackPressed() {
         super.onBackPressed()
         overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right)
-
     }
-
 }
